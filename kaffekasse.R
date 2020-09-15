096fe8a3219128e3bb6dc8486fb989ba2544ae5f
ManuelStapper <30607899+ManuelStapper@users.noreply.github.com>
2020-09-15 11:30:27 +0200

    Update kaffekasse.R

--- kaffekasse.R
+++ kaffekasse.R
@@ -5,6 +5,7 @@ setwd("~/Work/Verwaltung/Kaffeekasse")
 
 as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 einnahmen <- rbind.data.frame(
+  c("15.09.2020", "Manuel", 40),
   c("15.09.2020", "Andreas",    50),
   c("15.09.2020", "Willi",      15),
   c("2019", "Andreas",    11+20),
