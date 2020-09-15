7b8ea6d1d9c055e4bb25621a3639a25f94433448
Willi Mutschler <willi@mutschler.eu>
2020-09-15 10:45:06 +0200

    Relaunch

--- README.md
+++ README.md
@@ -1,59 +1,36 @@
-**Aktueller Stand: -34 Euro**
+**Aktueller Stand: -96 Euro**
 
 
-|Ausgabe         |Betrag |
-|:---------------|:------|
-|Kaffee          |109    |
-|Milch           |8      |
-|Milch           |8      |
-|Milch           |8      |
-|Kaffee          |96     |
-|Milch           |8      |
-|Milch           |8      |
-|Wasserbehaelter |20     |
-|Kaffee          |118    |
-|Milch           |8      |
-|Kaffee          |20     |
-|Milch           |8      |
-|Milch           |8      |
-|Milch           |8      |
-|Kaffee          |99     |
-|Kaffee          |78     |
-|Kaffee          |30     |
-|Reinigungskram  |19     |
+|Datum      |Ausgabe                      |Betrag |
+|:----------|:----------------------------|:------|
+|15.09.2020 |Kaffee 10kg (real)           |80     |
+|15.09.2020 |Wasserfilter (real)          |37     |
+|15.09.2020 |Entkalker (amazon)           |10     |
+|2017-2019  |Kaffee+Milch+Reinigungskrams |661    |
 
 
 |Datum      |Name       |Betrag |
 |:----------|:----------|:------|
-|07.10.2017 |Till       |20     |
-|11.10.2017 |Martina    |20     |
-|11.10.2017 |Willi      |30     |
-|16.10.2017 |Friederike |20     |
-|16.10.2017 |Fabian     |20     |
-|17.10.2017 |Andreas    |20     |
-|03.11.2017 |Manuel     |20     |
-|08.11.2017 |Sinem      |20     |
-|30.01.2018 |Martina    |10     |
-|13.02.2018 |Andreas    |10     |
-|13.02.2018 |Manuel     |10     |
-|13.02.2018 |Friederike |10     |
-|15.02.2018 |Till       |10     |
-|18.02.2018 |Willi      |20     |
-|18.02.2018 |Kevin      |30     |
-|18.02.2018 |Martina    |30     |
-|04.06.2018 |Manuel     |20     |
-|05.06.2018 |Andreas    |20     |
-|05.06.2018 |Willi      |30     |
-|05.06.2018 |Friederike |20     |
-|05.06.2018 |Hauke      |20     |
-|05.06.2018 |Till       |20     |
-|05.06.2018 |Kevin      |20     |
-|05.06.2018 |Andreas    |20     |
-|03.02.2019 |Andreas    |11     |
-|03.02.2019 |Friederike |11     |
-|05.02.2019 |Hauke      |20     |
-|05.02.2019 |Willi      |20     |
-|01.08.2019 |Friederike |20     |
-|01.08.2019 |Kevin      |35     |
-|01.08.2019 |Andreas    |20     |
-|05.08.2019 |Willi      |20     |
+|15.09.2020 |Andreas    |50     |
+|15.09.2020 |Willi      |15     |
+|2019       |Andreas    |31     |
+|2019       |Friederike |31     |
+|2019       |Hauke      |20     |
+|2019       |Kevin      |35     |
+|2019       |Willi      |40     |
+|2018       |Andreas    |50     |
+|2018       |Friederike |30     |
+|2018       |Hauke      |20     |
+|2018       |Kevin      |50     |
+|2018       |Manuel     |30     |
+|2018       |Martina    |40     |
+|2018       |Till       |20     |
+|2018       |Willi      |50     |
+|2017       |Andreas    |20     |
+|2017       |Fabian     |20     |
+|2017       |Friederike |20     |
+|2017       |Manuel     |20     |
+|2017       |Martina    |20     |
+|2017       |Sinem      |20     |
+|2017       |Till       |30     |
+|2017       |Willi      |30     |
--- kaffekasse.R
+++ kaffekasse.R
@@ -1,65 +1,43 @@
 library(knitr)
-library(rChoiceDialogs)
-setwd(rchoose.dir())
+#library(rChoiceDialogs)
+#setwd(rchoose.dir())
+setwd("~/Work/Verwaltung/Kaffeekasse")
 
 as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 einnahmen <- rbind.data.frame(
-  c("07.10.2017", "Till",	      20),
-  c("11.10.2017", "Martina",    20),
-  c("11.10.2017", "Willi",	    30),	
-  c("16.10.2017", "Friederike", 20),
-  c("16.10.2017", "Fabian",	    20),
-  c("17.10.2017", "Andreas",    20),			
-  c("03.11.2017", "Manuel",	    20),			
-  c("08.11.2017", "Sinem",	    20),			
-  c("30.01.2018", "Martina",    10),			
-  c("13.02.2018", "Andreas",    10),			
-  c("13.02.2018", "Manuel",	    10),			
-  c("13.02.2018", "Friederike",	10),			
-  c("15.02.2018", "Till",	      10),			
-  c("18.02.2018", "Willi",      20),
-  c("18.02.2018", "Kevin",      30),
-  c("18.02.2018", "Martina",    30),
-  c("04.06.2018", "Manuel",     20),
-  c("05.06.2018", "Andreas",    20),
-  c("05.06.2018", "Willi",      30),
-  c("05.06.2018", "Friederike", 20),
-  c("05.06.2018", "Hauke",      20),
-  c("05.06.2018", "Till",       20),
-  c("05.06.2018", "Kevin",      20),
-  c("05.06.2018", "Andreas",    20),
-  c("03.02.2019", "Andreas",    11),
-  c("03.02.2019", "Friederike", 11),
-  c("05.02.2019", "Hauke",      20),
-  c("05.02.2019", "Willi",      20),
-  c("01.08.2019", "Friederike", 20),
-  c("01.08.2019", "Kevin",      35),
-  c("01.08.2019", "Andreas",    20),
-  c("05.08.2019", "Willi",      20)
+  c("15.09.2020", "Andreas",    50),
+  c("15.09.2020", "Willi",      15),
+  c("2019", "Andreas",    11+20),
+  c("2019", "Friederike", 11+20),
+  c("2019", "Hauke",      20),
+  c("2019", "Kevin",      35),
+  c("2019", "Willi",      20+20),
+  c("2018", "Andreas",    10+20+20),			
+  c("2018", "Friederike",	10+20),			
+  c("2018", "Hauke",      20),
+  c("2018", "Kevin",      30+20),
+  c("2018", "Manuel",	    10+20),			
+  c("2018", "Martina",    10+30),
+  c("2018", "Till",       20),
+  c("2018", "Willi",      20+30),
+  c("2017", "Andreas",    20),			
+  c("2017", "Fabian",	    20),
+  c("2017", "Friederike", 20),
+  c("2017", "Manuel",	    20),			
+  c("2017", "Martina",    20),
+  c("2017", "Sinem",	    20),			
+  c("2017", "Till",	      20+10),			
+  c("2017", "Willi",	    30)
 )
 colnames(einnahmen) <- c("Datum","Name","Betrag")
 
 ausgaben <- rbind.data.frame(
-  c("Kaffee", 109),
-  c("Milch",  8),
-  c("Milch",  8),
-  c("Milch",  8),
-  c("Kaffee", 96),
-  c("Milch",  8),
-  c("Milch",  8),
-  c("Wasserbehaelter", 20),
-  c("Kaffee", 118),
-  c("Milch",8),
-  c("Kaffee", 20),
-  c("Milch",8),
-  c("Milch",8),
-  c("Milch",8),
-  c("Kaffee", 99),
-  c("Kaffee", 78),
-  c("Kaffee", 30),
-  c("Reinigungskram", 19)
-  )
-colnames(ausgaben) <- c("Ausgabe","Betrag")
+  c("15.09.2020", "Kaffee 10kg (real)", 80),
+  c("15.09.2020", "Wasserfilter (real)", 37),
+  c("15.09.2020", "Entkalker (amazon)", 10),
+  c("2017-2019", "Kaffee+Milch+Reinigungskrams", 109+96+118+20+99+78+30 + 8*9 + 20+19)
+)
+colnames(ausgaben) <- c("Datum","Ausgabe","Betrag")
 stand <- paste("**Aktueller Stand:", sum(as.numeric.factor(einnahmen$Betrag))-sum(as.numeric.factor(ausgaben$Betrag)),
 "Euro**")
 print(stand)
